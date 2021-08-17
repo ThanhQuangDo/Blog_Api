f9c697da3c09afc57b15bf89a0238efd752096da
Thanh Quang Do <69701708+ThanhQuangDo@users.noreply.github.com>
2021-08-17 20:00:29 +0700

    create factory

--- app/Model/Customer.php
+++ app/Model/Customer.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
-    //
+    public $timestamps = false;
+    protected $fillable = [
+        'name_customer', 'phone_customer', 'address_customer', 'email_customer', 'city_customer'
+    ];
+    
 }
